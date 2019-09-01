5bb7ce5a10fc2a2c15efe887929625a901aa8bcc
nynniaw12 <aybars.ari@icloud.com>
2019-09-01 18:15:08 +0300

    kod

---
+++ smartclassmuadil/lib/assets.dart
@@ -0,0 +1,102 @@
+
+import 'package:flutter/material.dart';
+
+//Asset 1
+class AcilogoAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = new AssetImage('assets/images/FormResource.png');
+    Image image = Image(image: assetImage);
+    return Container(
+      child: image,
+    );
+  }
+}
+//Asset 2
+class AcilogoAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = new AssetImage('assets/images/FormResource.png');
+    Image image = Image(image: assetImage);
+    return Container(
+      child: image,
+    );
+  }
+}
+//Asset 3
+class AcilogoAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = new AssetImage('assets/images/FormResource.png');
+    Image image = Image(image: assetImage);
+    return Container(
+      child: image,
+    );
+  }
+}
+//Asset 4
+class AcilogoAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = new AssetImage('assets/images/FormResource.png');
+    Image image = Image(image: assetImage);
+    return Container(
+      child: image,
+    );
+  }
+}
+//Asset 5
+class AcilogoAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = new AssetImage('assets/images/FormResource.png');
+    Image image = Image(image: assetImage);
+    return Container(
+      child: image,
+    );
+  }
+}
+//Asset 6
+class AcilogoAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = new AssetImage('assets/images/FormResource.png');
+    Image image = Image(image: assetImage);
+    return Container(
+      child: image,
+    );
+  }
+}
+//Asset 7
+class AcilogoAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = new AssetImage('assets/images/FormResource.png');
+    Image image = Image(image: assetImage);
+    return Container(
+      child: image,
+    );
+  }
+}
+//Asset 8
+class AcilogoAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = new AssetImage('assets/images/FormResource.png');
+    Image image = Image(image: assetImage);
+    return Container(
+      child: image,
+    );
+  }
+}
+//Asset 9
+class AcilogoAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = new AssetImage('assets/images/FormResource.png');
+    Image image = Image(image: assetImage);
+    return Container(
+      child: image,
+    );
+  }
+}
---
+++ smartclassmuadil/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:smartclassmuadil/assets.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title:'Zame',
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Zame Powerclass"),
+      ),
+      body: SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          
+
+
+
+        ]
+      ),
+      ),
+  )
+    )
+    );
+}
